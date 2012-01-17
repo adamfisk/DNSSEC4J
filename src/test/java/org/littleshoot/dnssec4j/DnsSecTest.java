d80275b8023fd9392f314e71b037d41523d2f227
Adam Fisk <a@littleshoot.org>
2012-01-16 16:04:52 -0800

    Lots of changes, including adding recursive calls on DS records

--- src/test/java/org/littleshoot/dnssec4j/DnsSecTest.java
+++ src/test/java/org/littleshoot/dnssec4j/DnsSecTest.java
@@ -1,6 +1,7 @@
 package org.littleshoot.dnssec4j;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
 
 import org.junit.Test;
@@ -20,6 +21,7 @@ import org.xbill.DNS.RRSIGRecord;
 import org.xbill.DNS.RRset;
 import org.xbill.DNS.Record;
 import org.xbill.DNS.Resolver;
+import org.xbill.DNS.ResolverConfig;
 import org.xbill.DNS.Section;
 import org.xbill.DNS.SimpleResolver;
 import org.xbill.DNS.Type;
@@ -29,6 +31,14 @@ public class DnsSecTest {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
     
+    {
+    //System.setProperty("sun.net.spi.nameservice.nameservers", "8.8.8.8,8.8.4.4");
+    //System.setProperty("sun.net.spi.nameservice.nameservers", "75.75.75.75 ,75.75.75.76");
+        
+    // These are from https://www.dns-oarc.net/oarc/services/odvr
+    System.setProperty("sun.net.spi.nameservice.nameservers", "149.20.64.20,149.20.64.21");
+    }
+    
     @Test
     public void testDNSKEYLookup() throws Exception {
         final String site = "www.verisign.com";
@@ -37,7 +47,11 @@ public class DnsSecTest {
         verifyFullRecord(full);
     }
 
-    private void verifyFullRecord(final Name full) throws IOException {
+    private void verifyFullRecord(final Name full) throws IOException, 
+        DNSSECException {
+        System.out.println("Verifying record: "+ full);
+        final String [] servers = ResolverConfig.getCurrentConfig().servers();
+        System.out.println(Arrays.asList(servers));
         final Resolver res = new ExtendedResolver();
         res.setEDNS(0, 0, ExtendedFlags.DO, null);
         res.setTCP(true);
@@ -45,13 +59,14 @@ public class DnsSecTest {
         final Record question = Record.newRecord(full, Type.A, DClass.IN);
         final Message query = Message.newQuery(question);
         final Message response = res.send(query);
-        //System.out.println("RESPONSE: "+response);
+        System.out.println("RESPONSE: "+response);
         final RRset[] answer = response.getSectionRRsets(Section.ANSWER);
         
+        //System.out.println("Answers: "+Arrays.asList(answer));
         // TODO: Verify all sets!
-        //for (final RRset set : answer) {
-        for (int i = 0; i < 1; i++) {
-            final RRset set = answer[i];
+        for (final RRset set : answer) {
+        //for (int i = 0; i < 1; i++) {
+            //final RRset set = answer[i];
             System.out.println("\n;; RRset to chase:");
             //System.out.println(set);
             final Iterator<Record> rrIter = set.rrs();
@@ -60,77 +75,114 @@ public class DnsSecTest {
                 System.out.println(rec);
             }
             final Iterator<Record> sigIter = set.sigs();
-            RRSIGRecord rec = null;
+            //if (!sigIter.hasNext()) {
+            //    throw new IOException("No sig records?");
+            //}
+            //RRSIGRecord rec = null;
             while (sigIter.hasNext()) {
-                rec = (RRSIGRecord) sigIter.next();
+                final RRSIGRecord rec = (RRSIGRecord) sigIter.next();
                 System.out.println("\n;; RRSIG of the RRset to chase:");
                 System.out.println(rec);
-                
-                System.out.println("\nLaunch a query to find a RRset of type DNSKEY for zone: "+rec.getSigner());
-                try {
-                    verifySig(set, rec);
-                    
-                    verifyDsRecordForSignerOf(rec);
-                    
-                } catch (final DNSSECException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+                verifyZone(set, rec);
             }
         }
     }
 
-    private void verifyDsRecordForSignerOf(final RRSIGRecord rec) 
+    private void verifyZone(final RRset set, final RRSIGRecord record) 
         throws IOException, DNSSECException {
-        final Name signer = rec.getSigner();
-        System.out.println("\nLaunch a query to find a RRset of type DS for zone: "+signer);
-        final Resolver res = new ExtendedResolver();
-        res.setEDNS(0, 0, ExtendedFlags.DO, null);
-        res.setTCP(true);
-        
-        // Timeouts are in seconds.
-        res.setTimeout(40);
-        
-        final Record question = Record.newRecord(signer, Type.DS, DClass.IN);
-        final Message query = Message.newQuery(question);
-        final Message response = res.send(query);
-        //System.out.println("DS RESPONSE:\n**************\n"+response+"\n**************\n");
-        
-        final RRset[] answer = response.getSectionRRsets(Section.ANSWER);
-        for (final RRset set : answer) {
-            //System.out.println(set);
-            final Iterator<Record> rrIter = set.rrs();
-            System.out.println("\n;; DSset of the DNSKEYset");
-            while (rrIter.hasNext()) {
-                System.out.println(rrIter.next());
-            }
-            final Iterator<Record> sigIter = set.sigs();
-            System.out.println("\n;; RRSIG of the DSset of the DNSKEYset");
-            while (sigIter.hasNext()) {
-                final Record sigRec = sigIter.next();
-                System.out.println(sigRec);
-                if (sigRec instanceof RRSIGRecord) {
-                    final RRSIGRecord rr = (RRSIGRecord) sigRec;
-                    System.out.println("\n;; Verifying signature of DS record");
-                    verifySig(set, rr);
-                } else {
-                    //System.out.println("non-sig record");
-                    //System.out.println(sigRec);
-                }
-            }
-        }
-    }
+        System.out.println("\nLaunch a query to find a RRset of type DNSKEY for zone: "+record.getSigner());
 
-    private void verifySig(final RRset set, final RRSIGRecord rrsig) 
-        throws IOException, DNSSECException {
-        //System.out.println("FOOTPRINT: " + rrsig.getFootprint());
-        final DNSKEYRecord publicKey = keyForRecord (rrsig);
-        DNSSEC.verify(set, rrsig, publicKey);
-    }
-
-    private DNSKEYRecord keyForRecord(final RRSIGRecord record) throws IOException {
+        //System.out.println("\nVerifying sig for rec: "+record);
+        //final DNSKEYRecord publicKey = signingKeyForRecord (record);
+        
         final Name signer = record.getSigner();
         final int tag = record.getFootprint();
+        System.out.println("Looking for tag: "+tag);
+        
+        boolean keyVerified = false;
+        DNSKEYRecord keyRec = null;
+        RRset recursiveSet = null;
+        RRSIGRecord recursiveSig = null;
+        
+        // We need to perform a multiline query to get the tags associated with
+        // keys, which lets us verify records with the correct key.
+        try {
+            Options.set("multiline");
+            final Resolver res = new ExtendedResolver();
+            res.setEDNS(0, 0, ExtendedFlags.DO, null);
+            res.setTCP(true);
+            
+            // Timeouts are in seconds.
+            res.setTimeout(40);
+            
+            final Record question = Record.newRecord(signer, Type.DNSKEY, DClass.IN);
+            final Message query = Message.newQuery(question);
+            final Message response = res.send(query);
+            
+            final RRset[] answer = response.getSectionRRsets(Section.ANSWER);
+            for (final RRset answerSet : answer) {
+                System.out.println("\n;; DNSKEYset that signs the RRset to chase:");
+                //System.out.println(set);
+                final Iterator<Record> rrIter = answerSet.rrs();
+                while (rrIter.hasNext()) {
+                    final Record rec = rrIter.next();
+                    System.out.println(rec);
+                    if (rec instanceof DNSKEYRecord) {
+                        final DNSKEYRecord dnskKeyRec = (DNSKEYRecord) rec;
+                        if (dnskKeyRec.getFootprint() == tag) {
+                            System.out.println("\n\nFound matching DNSKEY for tag!! "+tag+"\n\n");
+                            keyRec = dnskKeyRec;
+                        }
+                    }
+                }
+                System.out.println("\n;; RRSIG of the DNSKEYset that signs the RRset to chase:");
+                final Iterator<Record> sigIter = answerSet.sigs();
+                while (sigIter.hasNext()) {
+                    final RRSIGRecord rec = (RRSIGRecord) sigIter.next();
+                    System.out.println(rec);
+                    
+                    // This resource record set will be self-signed. Verify
+                    // the signature as we go, and we'll validate the DS record
+                    // as well later.
+                    if (rec.getFootprint() == tag) {
+                        System.out.println("Found matching footprint/tag!! "+tag);
+                        DNSSEC.verify(answerSet, rec, keyRec);
+                        keyVerified = true;
+                        recursiveSet = answerSet;
+                        recursiveSig = rec;
+                    }
+                }
+            }
+            if (!keyVerified) {
+                System.out.println("KEY NOT VERIFIED!!");
+                //throw new IOException("Key not verified!!");
+            }
+            if (keyRec == null) {
+                throw new IOException("Did not find DNSKEY record matching tag: "+tag);
+            }
+            //return keyRec;
+        } finally {
+            Options.unset("multiline");
+        }
+        DNSSEC.verify(set, record, keyRec);
+        
+        verifyDsRecordForSignerOf(record);
+    }
+
+    private DNSKEYRecord verifySig(final RRset set, final RRSIGRecord rrsig) 
+        throws IOException, DNSSECException {
+        //System.out.println("FOOTPRINT: " + rrsig.getFootprint());
+        final DNSKEYRecord publicKey = signingKeyForRecord (rrsig);
+        DNSSEC.verify(set, rrsig, publicKey);
+        return publicKey;
+    }
+
+    private DNSKEYRecord signingKeyForRecord(final RRSIGRecord record) 
+        throws IOException, DNSSECException {
+        final Name signer = record.getSigner();
+        final int tag = record.getFootprint();
+        
+        boolean keyVerified = false;
         // We need to perform a multiline query to get the tags associated with
         // keys, which lets us verify records with the correct key.
         try {
@@ -162,13 +214,23 @@ public class DnsSecTest {
                         }
                     }
                 }
-                // TODO: Verify the signature of the DNSKEY rrset itself.
                 System.out.println("\n;; RRSIG of the DNSKEYset that signs the RRset to chase:");
                 final Iterator<Record> sigIter = set.sigs();
                 while (sigIter.hasNext()) {
                     final RRSIGRecord rec = (RRSIGRecord) sigIter.next();
                     System.out.println(rec);
+                    
+                    // This resource record set will be self-signed. Verify
+                    // the signature as we go, and we'll validate the DS record
+                    // as well later.
+                    if (rec.getFootprint() == tag) {
+                        DNSSEC.verify(set, rec, keyRec);
+                        keyVerified = true;
+                    }
                 }
+            }
+            if (!keyVerified) {
+                throw new IOException("Key not verified!!");
             }
             if (keyRec == null) {
                 throw new IOException("Did not find DNSKEY record matching tag: "+tag);
@@ -177,6 +239,57 @@ public class DnsSecTest {
         } finally {
             Options.unset("multiline");
         }
+    }
+    
+
+    private void verifyDsRecordForSignerOf(final RRSIGRecord rec) 
+        throws IOException, DNSSECException {
+        final Name signer = rec.getSigner();
+        System.out.println("\nLaunch a query to find a RRset of type DS for zone: "+signer);
+        final Resolver res = new ExtendedResolver();
+        res.setEDNS(0, 0, ExtendedFlags.DO, null);
+        res.setTCP(true);
+        
+        // Timeouts are in seconds.
+        res.setTimeout(40);
+        
+        final Record question = Record.newRecord(signer, Type.DS, DClass.IN);
+        final Message query = Message.newQuery(question);
+        final Message response = res.send(query);
+        System.out.println("DS RESPONSE:\n**************\n"+response+"\n**************\n");
+        
+        final RRset[] answer = response.getSectionRRsets(Section.ANSWER);
+        for (final RRset set : answer) {
+            //System.out.println(set);
+            final Iterator<Record> rrIter = set.rrs();
+            System.out.println("\n;; DSset of the DNSKEYset");
+            while (rrIter.hasNext()) {
+                System.out.println(rrIter.next());
+            }
+            final Iterator<Record> sigIter = set.sigs();
+            System.out.println("\n;; RRSIG of the DSset of the DNSKEYset");
+            while (sigIter.hasNext()) {
+                final Record sigRec = sigIter.next();
+                System.out.println(sigRec);
+                if (sigIter.hasNext()) {
+                    throw new IOException("We don't handle more than one RRSIGRecord for DS responses!!");
+                }
+                if (sigRec instanceof RRSIGRecord) {
+                    final RRSIGRecord rr = (RRSIGRecord) sigRec;
+                    
+                    //System.out.println("VERIFYING DS RECORD -- RECURSIVE CALL!!");
+                    System.out.println(";; Now, we want to validate the DS :  recursive call");
+                    verifyZone(set, rr);
+                    //System.out.println("\n;; Verifying signature of DS record");
+                    //verifySig(set, rr);
+                } else {
+                    //System.out.println("non-sig record");
+                    //System.out.println(sigRec);
+                }
+            }
+        }
+        
+        System.out.println(";; Out of recursive call");
     }
 
     private DNSKEYRecord getRootKey() throws DNSSECException, IOException {
